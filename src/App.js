3972159695d3cd4ea8316f83204f586d42c88ec9
AndreyHerzhov <andreyherzhov@gmail.com>
2022-07-20 12:30:26 +0300

    first_render

--- src/App.js
+++ src/App.js
@@ -1,15 +1,3 @@
-import PaintingList from './components/PaintingList';
-import Section from './components/Section';
-import paintings from './paintings.json';
-
-export default function App() {
-  return (
-    <div>
-      <Section title="Топ недели">
-        <PaintingList items={paintings} />
-      </Section>
-
-      <Section title="Лучшее"></Section>
-    </div>
-  );
-}
+ function App(){
+  return <div>...</div>
+ }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import paintings from './paintings.json'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+function Painting(props) {
+  const {url, title, profileUrl, authorName, price} = props
+  console.log(props)
+  return (
+      <div>
+        <img src={url} alt={title} width="480" />
+        <h2>{title}</h2>
+        <p>
+          Автор: <a href={profileUrl}>{authorName}</a>
+        </p>
+        <p>Цена: {price}</p>
+        <p>Доступность: заканчивается или есть в наличии</p>
+        <button type="button">Добавить в корзину</button>
+      </div>
+    
+  )
+}
+
+const painting = paintings[1]
+
+ReactDOM.render(<Painting 
+          url={painting.url} 
+          title={painting.title} 
+          authorName={painting.author.tag}
+          profileUrl={painting.author.url}
+          price={painting.price}
+  />, document.querySelector('#root'))
+
+
